6064e4022974ff8a6bd2e4c27eddc351608f5f58
JennerMaxim <jennersi1remaxim@gmail.com>
2023-05-07 23:42:47 +0300

    Creating the Register file / Sign Up form

---
+++ src/pages/Register.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Add from "../img/addAvatar.png"
+
+function Register() {
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Maxim Chat App</span>
+        <span className="title">Register</span>
+        <form>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input style={{display:"none"}} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="Profil"/>
+            <span>Add a profil</span>
+          </label>
+          <button>Sign Up</button>
+        </form>
+        <p>You do have an account? Login</p>
+      </div>
+    </div>
+  );
+}
+
+export default Register;
