d71f213d8c9fa75acc6e7e2f270466a373b1f635
JennerMaxim <jennersi1remaxim@gmail.com>
2023-05-29 19:42:21 +0300

    Sending Data from Register to Firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA3iHGBeFjAea7hcLpbrNkwUzD5bOVO_hs",
-  authDomain: "chat-70551.firebaseapp.com",
-  projectId: "chat-70551",
-  storageBucket: "chat-70551.appspot.com",
-  messagingSenderId: "861278006292",
-  appId: "1:861278006292:web:76af645578bc2f66e8cb01",
+  apiKey: "AIzaSyBvG7h6o0_mTEBlCKkdBN8-r53uAMgcuCM",
+  authDomain: "chat-aa314.firebaseapp.com",
+  projectId: "chat-aa314",
+  storageBucket: "chat-aa314.appspot.com",
+  messagingSenderId: "397913721714",
+  appId: "1:397913721714:web:dec7c768d1ac5b6abc6beb",
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,27 +1,72 @@
 import React from "react";
-import Add from "../img/addAvatar.png"
+import Add from "../img/addAvatar.png";
+import { auth, storage, db } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { useState } from "react";
+import { doc, setDoc } from "firebase/firestore";
+import { ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 
-function Register() {
+const Register = () => {
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+    
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      const storageRef = ref(storage, displayName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            /* await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            }); */
+
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="formContainer">
       <div className="formWrapper">
         <span className="logo">Maxim Chat App</span>
         <span className="title">Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Name" />
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
-          <input style={{display:"none"}} type="file" id="file" />
+          <input style={{ display: "none" }} type="file" id="file" />
           <label htmlFor="file">
-            <img src={Add} alt="Profil"/>
+            <img src={Add} alt="Profil" />
             <span>Add a profil</span>
           </label>
           <button>Sign Up</button>
+          {err && <span>Something went wrong!</span>}
         </form>
         <p>You do have an account? Login</p>
       </div>
     </div>
   );
-}
+};
 
 export default Register;
