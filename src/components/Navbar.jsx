5faaa46170ea94bc9b1c0f4f6298050a5ef2009b
JennerMaxim <jennersi1remaxim@gmail.com>
2023-12-25 14:07:13 +0300

    Get the User profile from the database and set it in the front-end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,19 @@
-import React from "react";
-import profile from "../img/profile.png";
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 function Navbar() {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="navbar">
       <span className="logo">Maxim Chat App</span>
       <div className="user">
-        <img src={profile} alt="Profile" />
-        <span>Maxim</span>
+        <div className="img-name">
+          <img src={currentUser.photoURL} alt="Profile" />
+          <span>{currentUser.displayName}</span>
+        </div>
         <button onClick={() => signOut(auth)}>Logout</button>
       </div>
     </div>
