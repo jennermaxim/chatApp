4cfe1c3e3c65f3371ce92467227e26a410ddb510
JennerMaxim <jennersi1remaxim@gmail.com>
2023-12-25 16:25:14 +0300

    Fetch User from the database and display them in the side bar

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,32 +1,39 @@
-import React from "react";
-import profile from "../img/profile.png";
+import React, { useContext, useEffect, useState } from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
-function Chats() {
+const Chats = () => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+      return () => {
+        unsub();
+      };
+    };
+    currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  // console.log(Object.entries(chats));
+
   return (
     <div className="chats">
-      <div className="userChat">
-        <img src={profile} alt="Profile" />
-        <div className="userChatInfo">
-          <span>Maxim</span>
-          <p>Hello Maxim</p>
+      {Object.entries(chats)?.map((chat) => (
+        <div className="userChat" key={chat[0]}>
+          <img src={chat[1].userInfo.photoURL} alt="Profile" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo.displayName}</span>
+            <p>{chat[1].userInfo.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
-      <div className="userChat">
-        <img src={profile} alt="Profile" />
-        <div className="userChatInfo">
-          <span>Maxim</span>
-          <p>Hello Maxim</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img src={profile} alt="Profile" />
-        <div className="userChatInfo">
-          <span>Maxim</span>
-          <p>Hello Maxim</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
-}
+};
 
 export default Chats;
