f3ffd6e7f0353acf19b90412b95f8ee9304ff44e
jennermaxim <jennersi1remaxim@gmail.com>
2025-01-07 20:20:06 +0300

    Add the phone number and OTP generation for verification

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,21 +8,45 @@ import { doc, setDoc } from "firebase/firestore";
 import { Link, useNavigate } from "react-router-dom";
 
 const Register = () => {
-  const [err, setErr] = useState(false);
+  const [err, setErr] = useState(null);
   const navigate = useNavigate();
   const [disableRegister, setDisableRegister] = useState(false);
+
+  // OPT verification using SwiftOTP
+  // const [phoneNumber, setPhoneNumber] = useState("");
+  const [response, setResponse] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const displayName = e.target[0].value;
-    const email = e.target[1].value;
-    const password = e.target[2].value;
-    const file = e.target[3].files[0];
+    const contact = e.target[1].value;
+    const email = e.target[2].value;
+    const password = e.target[3].value;
+    const file = e.target[4].files[0];
 
     setDisableRegister(true);
 
     try {
+      // OTP verification using SwiftOTP
+      const opt = await fetch("https://swiftotp.xyz/api/v1/send/otp", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          apikey: "swft-0feab4ac-95d8-4d87-ac68-7b2871e98c50",
+        },
+        body: JSON.stringify({
+          phoneNumber: contact,
+          eventName: "api-testing",
+        }),
+      });
+      if (!opt.ok) {
+        setErr(`Error: ${opt.status}`);
+      }
+      const data = await opt.json();
+      setResponse(data);
+      setErr(null);
+
       // LOGIN AUTHENTIFICAION
       const res = await createUserWithEmailAndPassword(auth, email, password);
 
@@ -32,7 +56,7 @@ const Register = () => {
 
       uploadTask.on(
         (error) => {
-          setErr(true);
+          setErr(`Upload Error: ${error}`);
         },
         () => {
           getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
@@ -45,8 +69,10 @@ const Register = () => {
             await setDoc(doc(db, "users", res.user.uid), {
               uid: res.user.uid,
               displayName,
+              contact,
               email,
               photoURL: downloadURL,
+              response,
             });
 
             // USER CHAT
@@ -56,7 +82,8 @@ const Register = () => {
         }
       );
     } catch (err) {
-      setErr(true);
+      setErr(err.message || "Something went wrong!");
+      setResponse(null);
       setDisableRegister(false);
     }
   };
@@ -68,6 +95,7 @@ const Register = () => {
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input type="text" placeholder="Name" required />
+          <input type="tel" placeholder="Enter Phone Number" required />
           <input type="email" placeholder="Email" required />
           <input
             type="password"
@@ -84,9 +112,9 @@ const Register = () => {
             {disableRegister ? <img src={Loading} alt="" /> : "Sign Up"}
           </button>
           {disableRegister && (
-            <span>Please don't refresh or cancel, be patient with us 🤗</span>
+            <span>Please don't refresh or cancel</span>
           )}
-          {err && <span>The account already exist!</span>}
+          {err && <span style={{ color: "red" }}>{err}</span>}
         </form>
         <p>
           You do have an account? <Link to="/login">Login</Link>
